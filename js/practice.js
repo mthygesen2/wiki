188d3c3e4b5f7600938309a2067946d0b15aac88
Austin Minnon and Mao Thygesen <student@epicodus.com>
2016-01-14 16:04:59 -0800

    add 99 bottles loop

---
+++ js/practice.js
@@ -0,0 +1,21 @@
+$(document).ready(function(event){
+
+  // for(var index = 99; index >= 0; index -= 1){
+  //   $("#beer").append("<p>" + index + "bottles of beer on the wall" + "</p>");
+  // });
+  //
+  // $("#beerbutton").click(function(){
+  //   $("#beer").toggle().show();
+  //     event.preventDefault();
+  // });
+
+  for(var index = 99; index >= 0; index -= 1){
+      $(".beer").append("<p>"+index+" bottles of beer on the wall "+index+" bottles of beer. Take one down, pass it around" + index);
+  }
+  for(var index1 = 98; index1 >= 0; index1 -= 1) {
+      $(".beer").append(index1+" bottle of beer on the wall"+"</p>");
+    }
+    $("#99bofb").click(function() {
+      $(".beer").toggle();
+  });
+});
